2ebb4fc2e24c8d301d8205bdddf702d004062536
Lionel <Lionel.Delsupexhe@gmail.com>
2022-05-25 10:52:53 +0200

    create basic project architecture

---
+++ auth-service/src/main/java/be/technifutur/auth/AuthApplication.java
@@ -0,0 +1,11 @@
+package be.technifutur.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
