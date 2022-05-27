820331b88bd12707dc6e72831ac82354704e5056
sbstnlambert <sbstn.lambert@gmail.com>
2022-05-27 14:04:34 +0200

    added findByUserRef JPQL method

--- back-end-application/order-service/src/main/java/be/technifutur/order/repository/OrderRepository.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/repository/OrderRepository.java
@@ -3,5 +3,12 @@ package be.technifutur.order.repository;
 import be.technifutur.order.model.entity.OnlineOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface OrderRepository extends JpaRepository<OnlineOrder, Long> {
+
+    Optional<List<OnlineOrder>> findByUserRef(UUID userRef);
+
 }
