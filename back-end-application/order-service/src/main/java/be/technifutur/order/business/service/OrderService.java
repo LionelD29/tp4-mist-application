628341fa1a9040f98199c611e1a18d0dc4982014
sbstnlambert <sbstn.lambert@gmail.com>
2022-05-27 14:05:00 +0200

    added @AllArgsConstructor

--- back-end-application/order-service/src/main/java/be/technifutur/order/business/service/OrderService.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/business/service/OrderService.java
@@ -1,7 +1,10 @@
 package be.technifutur.order.business.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class OrderService {
+
 }
