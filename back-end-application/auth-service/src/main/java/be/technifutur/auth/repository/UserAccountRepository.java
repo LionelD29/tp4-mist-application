76ed74f77b3c7779a865f4e9f4313f773d4356b1
Lionel <Lionel.Delsupexhe@gmail.com>
2022-05-27 17:13:16 +0200

    implemented toggleUserAccount method (to activate/deactive an account)

--- back-end-application/auth-service/src/main/java/be/technifutur/auth/repository/UserAccountRepository.java
+++ back-end-application/auth-service/src/main/java/be/technifutur/auth/repository/UserAccountRepository.java
@@ -4,9 +4,11 @@ import be.technifutur.auth.model.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
     Optional<UserAccount> findByEmail(String email);
+    Optional<UserAccount> findByRef(UUID ref);
 
 }
