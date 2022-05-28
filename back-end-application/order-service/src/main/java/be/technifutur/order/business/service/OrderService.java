e1febf166b0a259c630f665d6769698324a71802
sbstnlambert <sbstn.lambert@gmail.com>
2022-05-28 17:50:29 +0200

    implemented placeOrder method

--- back-end-application/order-service/src/main/java/be/technifutur/order/business/service/OrderService.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/business/service/OrderService.java
@@ -2,6 +2,8 @@ package be.technifutur.order.business.service;
 
 import be.technifutur.order.business.mapper.OrderMapper;
 import be.technifutur.order.model.dto.OrderDTO;
+import be.technifutur.order.model.entity.Order;
+import be.technifutur.order.model.form.OrderForm;
 import be.technifutur.order.repository.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,8 +18,16 @@ public class OrderService {
     private final OrderRepository repository;
     private final OrderMapper mapper;
 
+    // CREATE NEW ORDER
+    public OrderDTO placeOrder(UUID userRef, OrderForm form) {
+        Order entity = mapper.formToEntity(form);
+        entity = repository.save(entity);
+        return mapper.entityToDTO(entity);
+    }
+
+    // READ USER'S ORDERS LIST
     public List<OrderDTO> getOrdersByUser(UUID userRef) {
-        return this.repository.findByUserRef(userRef)
+        return repository.findByUserRef(userRef)
                 .stream()
                 .map(mapper::entityToDTO)
                 .toList();
--- back-end-application/order-service/src/main/java/be/technifutur/order/controller/OrderController.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/controller/OrderController.java
@@ -2,9 +2,11 @@ package be.technifutur.order.controller;
 
 import be.technifutur.order.business.service.OrderService;
 import be.technifutur.order.model.dto.OrderDTO;
+import be.technifutur.order.model.form.OrderForm;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,16 @@ public class OrderController {
 
     private final OrderService service;
 
+    // CREATE NEW ORDER
+    @PostMapping("/add")
+    public OrderDTO placeOrder(Authentication auth, OrderForm form) {
+        return service.placeOrder((UUID) auth.getPrincipal(), form);
+    }
+
+    // READ USER'S ORDERS LIST
     @GetMapping
     public List<OrderDTO> getOrdersByUser(Authentication auth) {
-        return this.service.getOrdersByUser((UUID) auth.getPrincipal());
+        return service.getOrdersByUser((UUID) auth.getPrincipal());
     }
 
 }
