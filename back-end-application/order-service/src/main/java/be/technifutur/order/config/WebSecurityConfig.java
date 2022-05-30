18f14c97deaabe64b36559e096748bed4bd1dbed
sbstnlambert <sbstn.lambert@gmail.com>
2022-05-30 17:47:47 +0200

    configured WebSecurityConfig

--- back-end-application/order-service/src/main/java/be/technifutur/order/config/WebSecurityConfig.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/config/WebSecurityConfig.java
@@ -1,4 +1,32 @@
 package be.technifutur.order.config;
 
-public class WebSecurityConfig {
+import be.technifutur.shared.model.security.JwtValidationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtValidationFilter jwtValidationFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests().anyRequest().permitAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtValidationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
 }
