da7c5e5f35238cdebdf359e4a5b50248379f8f50
ManonKerrels <manon.kerrels@gmail.com>
2022-05-30 11:11:22 +0200

    BeanConfig and WebSecurityConfig adding, linked to shared module

---
+++ back-end-application/game-service/src/main/java/be/technifutur/game/config/BeanConfig.java
@@ -0,0 +1,20 @@
+package be.technifutur.game.config;
+
+import be.technifutur.shared.model.security.JwtValidationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public JwtValidationFilter jwtValidationFilter(){
+        return new JwtValidationFilter(restTemplate());
+    }
+}
