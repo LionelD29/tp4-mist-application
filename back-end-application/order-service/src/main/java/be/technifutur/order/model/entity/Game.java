bdfc183285d01f57a1a62c771d5e591530da96b5
sbstnlambert <sbstn.lambert@gmail.com>
2022-06-03 16:35:53 +0200

    added @Setter for real this time

--- back-end-application/order-service/src/main/java/be/technifutur/order/model/entity/Game.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/model/entity/Game.java
@@ -1,9 +1,6 @@
 package be.technifutur.order.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Positive;
