9a172ce1c14a9efd25e8745a2738ff723a645f86
sbstnlambert <sbstn.lambert@gmail.com>
2022-06-03 14:44:15 +0200

    added OrderControllerAdvisor

---
+++ back-end-application/order-service/src/main/java/be/technifutur/order/controller/OrderControllerAdvisor.java
@@ -0,0 +1,28 @@
+package be.technifutur.order.controller;
+
+import be.technifutur.order.exception.OrderNotFoundException;
+import be.technifutur.order.model.dto.ErrorDTO;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class OrderControllerAdvisor extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<Object> handlePrice404(OrderNotFoundException ex, HttpServletRequest req){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorDTO.builder()
+                        .message(ex.getMessage())
+                        .reference(ex.getRef().toString())
+                        .method(HttpMethod.resolve(req.getMethod()))
+                        .uri(req.getRequestURI())
+                        .build());
+    }
+
+}
