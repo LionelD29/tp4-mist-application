63e465de20198f6eca623fd66921c065b53e9332
sbstnlambert <sbstn.lambert@gmail.com>
2022-06-03 09:58:17 +0200

    removed @LoadBalanced

--- back-end-application/order-service/src/main/java/be/technifutur/order/config/OrderBeanConfig.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/config/OrderBeanConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 public class OrderBeanConfig {
 
     @Bean
-    @LoadBalanced
     public RestTemplate template() {
         return new RestTemplate();
     }
