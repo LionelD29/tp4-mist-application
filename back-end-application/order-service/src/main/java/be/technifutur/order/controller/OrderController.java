f6eb249e9ed81af259f2243c4f2d7b35429928ee
sbstnlambert <sbstn.lambert@gmail.com>
2022-06-03 16:32:38 +0200

    added @PreAuthorize on controller methods

--- back-end-application/order-service/src/main/java/be/technifutur/order/controller/OrderController.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/controller/OrderController.java
@@ -4,6 +4,7 @@ import be.technifutur.order.business.service.OrderService;
 import be.technifutur.order.model.dto.OrderDTO;
 import be.technifutur.order.model.form.OrderForm;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,18 +19,21 @@ public class OrderController {
     private final OrderService service;
 
     // CREATE NEW ORDER
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/add")
     public OrderDTO placeOrder(Authentication auth, OrderForm form) {
         return service.placeOrder((UUID) auth.getPrincipal(), form);
     }
 
     // READ ORDERS LIST OF A SPECIFIC USER
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public List<OrderDTO> getOrdersByUser(Authentication auth) {
         return service.getOrdersByUser((UUID) auth.getPrincipal());
     }
 
     // READ ONE ORDER OF A SPECIFIC USER
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{orderId}")
     public OrderDTO getOneUserOrderByOrderId(Authentication auth, @PathVariable Long orderId) {
         return service.getOneUserOrderByOrderId((UUID) auth.getPrincipal(), orderId);
@@ -42,6 +46,7 @@ public class OrderController {
 //    }
 
     // DELETE ONE USER ORDER BY ID
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{orderId}")
     public OrderDTO deleteUserOrderById(Authentication auth, Long orderId) {
         return service.deleteUserOrderById((UUID) auth.getPrincipal(), orderId);
