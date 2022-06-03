a42ab45d1807820d8aa1979722ee0e79f1470cb9
sbstnlambert <sbstn.lambert@gmail.com>
2022-06-03 16:33:32 +0200

    cleaned imports

--- back-end-application/order-service/src/main/java/be/technifutur/order/repository/OrderRepository.java
+++ back-end-application/order-service/src/main/java/be/technifutur/order/repository/OrderRepository.java
@@ -1,14 +1,9 @@
 package be.technifutur.order.repository;
 
-import be.technifutur.order.model.dto.OrderDTO;
 import be.technifutur.order.model.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
