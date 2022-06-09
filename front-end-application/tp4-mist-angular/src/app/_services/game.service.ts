f2d0e2d51c02d4c2805fc391cb79b7bd9c648cdb
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-06-09 16:42:31 +0200

    developerService completed

--- front-end-application/tp4-mist-angular/src/app/_services/game.service.ts
+++ front-end-application/tp4-mist-angular/src/app/_services/game.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Game } from '../_models/game.model';
 import { GameInsertForm } from '../_models/gameInsertForm.model';
