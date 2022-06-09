e2b538c518ec70bbe30504e6a84ea99b8a9d5618
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-06-09 14:31:13 +0200

    models added for game-service

--- front-end-application/tp4-mist-angular/src/app/_services/game.service.ts
+++ front-end-application/tp4-mist-angular/src/app/_services/game.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class GameService {
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
+
+
+  // --- GET ---
+  
+
+
 }
