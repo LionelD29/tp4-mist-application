0426cd5720370813f6013575032abdc530b9972d
sbstnlambert <sbstn.lambert@gmail.com>
2022-06-13 08:54:12 +0200

    added order service methods

--- front-end-application/tp4-mist-angular/src/app/_services/order.service.ts
+++ front-end-application/tp4-mist-angular/src/app/_services/order.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order, OrderForm } from '../_models/order.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  private BASE_URL: string = 'http://localhost:8080/orders';
+
+  constructor(private http: HttpClient) { }
+
+  public placeOrder(form: OrderForm): Observable<Order> {
+    return this.http.post<Order>(`${this.BASE_URL}/add`, form);
+  }
+
+  public getOrdersByUser(): Observable<Array<Order>> {
+    return this.http.get<Array<Order>>(`${this.BASE_URL}`);
+  }
+
+  public getOneUserOrderByOrderId(orderId: number): Observable<Order> {
+    return this.http.get<Order>(`${this.BASE_URL}/${orderId}`);
+  }
+
+  public deleteUserOrderById(orderId: number): Observable<Order> {
+    return this.http.delete<Order>(`${this.BASE_URL}/${orderId}`);
+  }
+  
 }
