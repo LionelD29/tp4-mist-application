c96786c1649f28e183f37170a19943789bdea3af
Lionel <Lionel.Delsupexhe@gmail.com>
2022-06-13 15:22:38 +0200

    fixed problem in game-service database filler

--- back-end-application/game-service/src/main/java/be/technifutur/game/utils/DatabaseFiller.java
+++ back-end-application/game-service/src/main/java/be/technifutur/game/utils/DatabaseFiller.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,11 +29,11 @@ public class DatabaseFiller implements InitializingBean {
 
 
     @Override
-    public void afterPropertiesSet() throws Exception {
+    public void afterPropertiesSet() {
         setUpListGame();
     }
 
-    private void setUpListGame() throws Exception{
+    private void setUpListGame() {
         Developer d = Developer.builder()
                 .reference(UUID.randomUUID())
                 .name("FromSoftware Inc.")
@@ -52,7 +51,8 @@ public class DatabaseFiller implements InitializingBean {
         Game g = Game.builder()
                 .reference(UUID.fromString("195e9333-81db-43d3-8506-ed9467d3c6c4"))
                 .title("Elden Ring")
-                .releaseDate(LocalDate.of(2022, 02, 25))
+                .imageUrl("image of ELDEN RING")
+                .releaseDate(LocalDate.of(2022, 2, 25))
                 .genres(List.of(Genre.RPG, Genre.SOLO))
                 .editor(e)
                 .developer(d)
@@ -76,7 +76,8 @@ public class DatabaseFiller implements InitializingBean {
         g = Game.builder()
                 .reference(UUID.fromString("68cfd22f-799d-452d-8a09-7a292c41f010"))
                 .title("Dune - Spice wars")
-                .releaseDate(LocalDate.of(2022, 04, 26))
+                .imageUrl("image of Dune - Spice Wars")
+                .releaseDate(LocalDate.of(2022, 4, 26))
                 .genres(List.of(Genre.STRATEGY, Genre.SOLO))
                 .editor(e)
                 .developer(d)
@@ -100,7 +101,8 @@ public class DatabaseFiller implements InitializingBean {
         g = Game.builder()
                 .reference(UUID.fromString("f15a67d5-65cb-40f7-87e5-0408d4fd5c28"))
                 .title("Fall Guys - Sixth edition")
-                .releaseDate(LocalDate.of(2020, 8, 04))
+                .imageUrl("image of Fall Guys - Sixth edition")
+                .releaseDate(LocalDate.of(2020, 8, 4))
                 .genres(List.of(Genre.MULTI, Genre.ACTION))
                 .editor(e)
                 .developer(d)
