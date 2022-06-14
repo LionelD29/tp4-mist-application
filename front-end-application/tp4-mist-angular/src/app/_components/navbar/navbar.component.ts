d6cfad12a5eedd80a457dfe51d567f8260985826
Chrisdev10 <delbeke.christophe@hotmail.com>
2022-06-14 14:45:37 +0200

    navbar done

--- front-end-application/tp4-mist-angular/src/app/_components/navbar/navbar.component.ts
+++ front-end-application/tp4-mist-angular/src/app/_components/navbar/navbar.component.ts
@@ -11,57 +11,120 @@ export class NavbarComponent implements OnInit {
 
   @HostListener('window:scroll') 
   onScroll(e: Event): void {
-    var navbar = document.getElementById("navbar");
+    const navbar = document.getElementById("navbar");
+    const body = document.getElementsByTagName("body")[0];
     if(window.scrollY > 300){ 
       if(navbar){
         navbar.style.backgroundColor = "#292929";
-        navbar.style.transition = 'background .5s';
       }
     }else{
-      if(navbar){
+      if(!this.isOpen){
+        if(navbar){
         navbar.style.backgroundColor = "transparent";
+        }
+      }else{
+        if(navbar){
+          navbar.style.backgroundColor = "#292929";  
+        }
+      }    
+    }
+  }
+  @HostListener('window:resize')
+  checkSize(e: Event):void{
+    const navbar_elem = document.getElementById("navbarContent");
+    if(window.innerWidth <= 1000 && !this.isOpen){
+      if(navbar_elem){
+        if(!navbar_elem.classList.contains("collapser")){
+          navbar_elem.classList.add("collapser");
+        }
+      }
+    }else{
+      if(navbar_elem){
+        navbar_elem.classList.remove("collapser");
       }
     }
   }
+  
+  isOpen:boolean = false;
   constructor(
     public authService: AuthService,
     private router: Router
   ) { }
   
   ngOnInit(): void {
+    this.resizeOnInit();
   }
 
   onClick(event: any): void {
+    this.openMenu();
     this.router.navigate([event.target.id]);
   }
 
   onSignIn(): void {
+    this.openMenu();
     this.router.navigate(['auth', 'sign-in']);
   }
   onSignUp(): void {
+    this.openMenu();
     this.router.navigate(['auth', 'sign-up']);
   }
 
   onSignOut(): void {
+    this.openMenu();
     this.authService.signOut();
   }
   
-  openMenu(){
-    const btn = document.getElementById("menu-btn");
+  navbarChanger(){
+    const body = document.getElementsByTagName("body")[0];
     const navbar = document.getElementById("navbar");
-    const checkbtn = document.getElementById("checkbtn") as HTMLInputElement | null; 
-    if(btn && checkbtn)
-      btn.classList.toggle('opened');
-      if(checkbtn){
-        checkbtn.checked = checkbtn.checked ? false : true;
-        if(checkbtn.checked){
-          navbar?.classList.add("navbarc");
-        }else{
-          navbar?.classList.remove("navbarc");
-
+    if(this.isOpen){
+      if(navbar){
+        if(!navbar.classList.contains("navbarc")){
+          navbar.style.backgroundColor = "#292929";
+          navbar.classList.add("navbarc");
+          body.style.overflow = "hidden";
         }
       }
-        
+    }else{
+      if(navbar){
+        navbar.style.backgroundColor = "transparent";
+        navbar.classList.remove("navbarc");
+      }
+      body.style.overflow = "auto"; 
+    }      
   }
 
+  openMenu(){
+    if(window.innerWidth < 1000){
+    this.isOpen = this.isOpen ? false : true;
+    const navbar_elem = document.getElementById("navbarContent");
+    const navbar_parent = document.getElementById("parentItem");
+    const btn = document.getElementById("menu-btn");
+    if(this.isOpen){
+      if(navbar_elem && navbar_parent){
+        navbar_elem.classList.remove("collapser");
+        navbar_elem.classList.add("navbarItems");
+        navbar_parent.classList.add("navparent")
+      }
+    }else{
+      if(navbar_elem && navbar_parent){
+        navbar_elem.classList.add("collapser");
+        navbar_elem.classList.remove("navbarItems");
+        navbar_parent.classList.remove("navparent")
+      }  
+    }
+    if(btn)
+      btn.classList.toggle('opened'); 
+    this.navbarChanger();
+    }
+  }
+  
+  resizeOnInit(){
+    if(window.innerWidth <= 1000){
+      const navbar_elem = document.getElementById("navbarContent");
+      if(navbar_elem){
+        navbar_elem.classList.add("collapser");
+      }
+    }
+  }
 }
