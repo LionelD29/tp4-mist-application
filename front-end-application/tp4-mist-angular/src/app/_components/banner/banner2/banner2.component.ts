c7161007e8811e4e5466b4e7474cb06dc1df1328
Chrisdev10 <delbeke.christophe@hotmail.com>
2022-06-16 16:31:40 +0200

    finish

--- front-end-application/tp4-mist-angular/src/app/_components/banner/banner2/banner2.component.ts
+++ front-end-application/tp4-mist-angular/src/app/_components/banner/banner2/banner2.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner2.component.scss']
 })
 export class Banner2Component implements OnInit {
+
   constructor() { }
 
   ngOnInit(): void {
